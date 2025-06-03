a9e1a5cdbb69cbea786a530e7933683c9b29a8ae
Tim <tim@MacBook-Pro-2.local>
2025-06-03 23:16:25 +0300

    update base url

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,5 +1,5 @@
 import { createAxiosClient } from "./axiosApiClient";
 
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const axiosApi = createAxiosClient(BASE_URL); // возвращает get, post, put, и тд
